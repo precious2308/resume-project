257091e248c97e6e888827d065fad43ecb6fb6e7
Precious Kabamba <145596654+precious2308@users.noreply.github.com>
2025-09-17 07:18:57 -0700

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,12 @@
+let myName="Precious";
+let title="Student";
+let experience="Public Speaker, debator, has not worked before";
+let education="Oral Roberts University";
+let skills=["Programming,cooking,sewing"];
+
+document.getElementById("my-name").textContent=myName;
+document.getElementById("title").textContent=title;
+document.getElementById("experience").textContent=experience;
+document.getElementById("education").textContent=education;
+document.getElementById("skills").textContent=skills;
+
